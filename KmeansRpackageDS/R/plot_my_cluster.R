eb8dd933d091e004c1923e83b8411c4f59892eea
Danyal1s <danyal1s@hotmail.com>
2025-03-08 09:10:14 -0500

    Update plot_my_cluster.R

--- KmeansRpackageDS/R/plot_my_cluster.R
+++ KmeansRpackageDS/R/plot_my_cluster.R
@@ -1,14 +1,13 @@
 #' The function plots the k means cluster
 #'
 #' This function takes in the x and y values to plot along with k-means calculated
-#' cluster list and returns a k-means cluster plot
+#' cluster list
 #'
 #'
 #'
 #' @param xy_data list of x and y values to plot \code{xy_data}
 #' @param cluster_data list of k-means calculated cluster data \code{cluster_data}
 #'
-#' @return clusterPlot returns a k-means cluster plot to render
 #'
 #' @keywords keywords
 #'
